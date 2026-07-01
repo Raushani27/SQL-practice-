e348a084c3d6983e1b6b991d13780d3af8dfadd4
raushani kumari <raushanijnv2016@gmail.com>
2026-07-02 00:17:34 +0530

    Create Lesson-01.sql

---
+++ SQLBolt/Lesson-01.sql
@@ -0,0 +1,15 @@
+-- SQL Bolt Lesson 1
+SELECT Title
+FROM movies;
+
+SELECT Director
+FROM movies;
+
+SELECT Title ,Director
+FROM movies;
+
+SELECT Title ,Year
+FROM movies;
+
+SELECT *
+FROM movies;
