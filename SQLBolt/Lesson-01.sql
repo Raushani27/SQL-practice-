76323a3c0fe26ca7c18f430f79254530c5f366f7
raushani kumari <raushanijnv2016@gmail.com>
2026-07-02 21:01:46 +0530

    Lesson-01.sql

--- SQLBolt/Lesson-01.sql
+++ SQLBolt/Lesson-01.sql
@@ -1,15 +1,54 @@
--- SQL Bolt Lesson 1
-SELECT Title
+/*
+=========================================================
+Project       : SQL Practice
+Platform      : SQLBolt
+Lesson        : 01
+Topic         : SELECT Statement
+Author        : Raushani Kumari
+Date          : July 2026
+=========================================================
+Description:
+This lesson covers the basics of retrieving data
+from a table using the SELECT statement.
+=========================================================
+*/
+
+-- Question 1
+-- Display all movie titles
+
+SELECT title
 FROM movies;
 
-SELECT Director
+----------------------------------------------------------
+
+-- Question 2
+-- Display all directors
+
+SELECT director
 FROM movies;
 
-SELECT Title ,Director
+----------------------------------------------------------
+-- Question 3
+-- Find the title and director of each film.
+
+SELECT title,
+       director
 FROM movies;
 
-SELECT Title ,Year
+---------------------------------------------------------
+
+-- Question 4
+-- Find the title and year of each film.
+
+SELECT title,
+       year
 FROM movies;
+
+---------------------------------------------------------
+
+-- Question 5
+-- Find all the information about each film.
 
 SELECT *
 FROM movies;
+
